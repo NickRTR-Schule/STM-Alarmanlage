7325910d709cdd6932144f37d624417b26ba2a8d
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-11-13 20:33:15 +0100

    lichtschranke, mikrofon und magnetschalter

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "PinNamesTypes.h"
 #include "mbed.h"
 #include "platform/mbed_thread.h"
 
@@ -6,24 +7,44 @@
 int main()
 {
     DigitalOut sirene(PC_0);
+    DigitalOut test(PC_3);
+
     DigitalIn button(PB_0);
     button.mode(PullDown);
+
+    DigitalIn magnet(PB_1);
+    magnet.mode(OpenDrain);
+
+    DigitalIn lichtschranke(PB_3);
+    lichtschranke.mode(PullDown);
+
+    DigitalIn mikrofon(PB_2);
+    mikrofon.mode(OpenDrain);
+
+    DigitalOut gelb(PC_1);
+    DigitalOut rot(PC_2);
+    DigitalOut blau(PC_3);
+
+    DigitalOut status(LED1);
 
     bool scharf = false;
 
     while (true) {
-        if (button == true) {
+        if (button == 0) {
             scharf = !scharf;
-            thread_sleep_for(20); // prellen verhindern
+            thread_sleep_for(20);
         }
 
-        while(button == true) {}
+        while (button == 0) {}
 
-        while (scharf == true) {
-            sirene = true;
-            thread_sleep_for(SIRENE_DELAY);
+        if (scharf == true) {
+            status = true;
+            if (magnet == 0 || lichtschranke == 1 || mikrofon == 0) {
+                sirene = true;
+            }
+        } else {
+            status = false;
             sirene = false;
-            thread_sleep_for(SIRENE_DELAY);
         }
     }
 }
